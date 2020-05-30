0e6915d3ad24c29e69de0bb4e1b6064196a137fd
s0215_new <s0215_new@218.50.253.133>
2020-05-31 02:46:11 +0900

    세션 리스너 추가 - sessionCreated, sessionDestoryed 이벤트 체크를 위한 리스너 추가

---
+++ myTemplate/src/main/java/com/my/common/session/SessionListener.java
@@ -0,0 +1,31 @@
+package com.my.common.session;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class SessionListener implements HttpSessionListener {
+
+    private final AtomicInteger activeSessions;
+
+    public SessionListener() {
+        super();
+ 
+        activeSessions = new AtomicInteger();
+    }
+ 
+    public int getTotalActiveSession() {
+        return activeSessions.get();
+    }
+ 
+    public void sessionCreated(final HttpSessionEvent event) {
+
+        activeSessions.incrementAndGet();
+    }
+
+    public void sessionDestroyed(final HttpSessionEvent event) {
+
+        activeSessions.decrementAndGet();
+    }
+}
