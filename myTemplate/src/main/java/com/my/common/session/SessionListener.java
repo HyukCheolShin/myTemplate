cd2ef9ab7094292cefd8c608b097a864b1861e44
s0215_new <s0215_new@218.50.253.133>
2020-05-31 02:53:21 +0900

    로그아웃 시간 저장 - sessiondestoryed 이벤트가 발생했을 때 sessionVO 정보가 존재하면 로그아웃 시간을 업데이트

--- myTemplate/src/main/java/com/my/common/session/SessionListener.java
+++ myTemplate/src/main/java/com/my/common/session/SessionListener.java
@@ -1,11 +1,24 @@
 package com.my.common.session;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.my.template.mapper.LoginMapper;
+
 public class SessionListener implements HttpSessionListener {
+ 
+    @SuppressWarnings("unused")
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass().getName());
 
     private final AtomicInteger activeSessions;
 
@@ -25,6 +38,25 @@ public class SessionListener implements HttpSessionListener {
     }
 
     public void sessionDestroyed(final HttpSessionEvent event) {
+
+    	HttpSession session = event.getSession();
+    	SessionVO sessionVO = (SessionVO)session.getAttribute("sessionVO");
+
+    	if(sessionVO!=null) {
+    		ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(session.getServletContext());
+    		LoginMapper loginMapper = (LoginMapper) ctx.getBean("loginMapper");
+    		
+    		String sessionId = event.getSession().getId();
+    		
+    		try {
+    			Map<String, String> loginMap = new HashMap<String, String>();
+    			loginMap.put("sessionId", sessionId);
+    			loginMapper.updateLogoutLog(loginMap);
+    		} catch (Exception e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    	}
 
         activeSessions.decrementAndGet();
     }
--- myTemplate/src/main/java/com/my/template/mapper/LoginMapper.java
+++ myTemplate/src/main/java/com/my/template/mapper/LoginMapper.java
@@ -9,4 +9,5 @@ public interface LoginMapper {
 	Map<String, String> loginUserValidation(Map<String, Object> param) throws Exception;
 	Map<String, String> login(Map<String, ?> param) throws Exception;
 	int insertLoginLog(Map<String, ?> param) throws Exception;
+	int updateLogoutLog(Map<String, String> param) throws Exception;
 }
